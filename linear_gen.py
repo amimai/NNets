e51ef538a563fa9661083cf39134e956775d0355
amimai <50600852+amimai@users.noreply.github.com>
2019-06-07 05:50:18 +0100

    Create linear_gen.py

---
+++ linear_gen.py
@@ -0,0 +1,10 @@
+####################################################
+# generating linear test data for quick prototyping#
+####################################################
+# goals:
+# make custom dataset
+# save to file
+# 
+# 
+# 
+
