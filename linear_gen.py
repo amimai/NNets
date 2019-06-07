e29c1759004e7c6d1047e5454f1f754a891dc385
amimai <50600852+amimai@users.noreply.github.com>
2019-06-07 06:28:45 +0100

    a

--- linear_gen.py
+++ linear_gen.py
@@ -8,3 +8,15 @@
 # 
 # 
 
+import numpy as np
+from random import randrange
+from math import cos
+
+def gen_data(amm):
+    x = np.zeros((amm, 1))
+    for i in range(amm):
+        x[i] = [cos(i+(randrange(-10,10)/10.))]  # norm,norm,geometric,fibinacci,exponential
+    return x
+
+
+print(gen_data(5))
